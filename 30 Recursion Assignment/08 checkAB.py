af6f5eed8dfd761f4cdfe40252e39a85bae46c42
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-01-03 20:02:52 +0530

    Update 08 checkAB.py

--- 30 Recursion Assignment/08 checkAB.py
+++ 30 Recursion Assignment/08 checkAB.py
@@ -1,13 +1,32 @@
-def func(s):
-    if len(s) == 1:
-        return s
+ef abb(s, flag, bflag):
+  if len(s) == 0:
+    return flag
 
-    elif s[0] == s[1]:
-        return s[0] + "*" + func(s[1:])
+  elif s[0] == "a":
+    flag = True
+    bflag = False
+    return abb(s[1:], flag, bflag)
 
+  elif s[0] == 'b' and s[1] == 'b':
+    if bflag == True:
+      return False
     else:
-        return s[0] + func(s[1:])
+      bflag = True
+      return abb(s[2:], flag, bflag)
+  
+  else:
+    return False
+
 
 
 s = input()
-print(func(s))
+flag = True
+bflag = False
+if s[0] == "a":
+  x = abb(s, flag, bflag)
+else:
+  x = 0
+if x:
+  print("true")
+else:
+  print("false")
